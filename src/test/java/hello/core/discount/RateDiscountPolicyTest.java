1c145e10df7d65b7a05dc0264690da97b7bc7298
Juno531 <rksk0531@naver.com>
2023-02-08 18:48:35 +0900

    commit

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -3,12 +3,10 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
 
@@ -31,7 +29,7 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP가 아니면 할인이 적욕되지 않아야 한다.")
     void vip_x() {
         //given
-        Member member = new Member(1L, "BASIC", Grade.BASIC);
+        Member member = new Member(1L, "BASIC", Grade.VIP);
 
         //when
         int discount = rateDiscountPolicy.discount(member, 10000);

59b76d7c123e1f2324ec02cbea92dff087397cab
Juno531 <rksk0531@naver.com>
2023-02-15 21:24:10 +0900

    commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -24,13 +25,14 @@ public class AppConfig {
         return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
     @Bean
-    public static MemoryMemberRepository getMemberRepository() {
+    public MemoryMemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
     @Bean
-    public static DiscountPolicy getDiscountPolicy() {
+    public DiscountPolicy getDiscountPolicy() {
         return new RateDiscountPolicy();
     }
+
 
 }
